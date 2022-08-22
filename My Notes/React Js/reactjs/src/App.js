749632d12fd115ef87c907e3cee531a5b3b42864
Rabbu-G <105190642+Rabbu-G@users.noreply.github.com>
2022-08-22 12:03:05 +0530

    .

--- My Notes/React Js/reactjs/src/App.js
+++ My Notes/React Js/reactjs/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className='app'>
 
-      {/* <ClassComponents /> */}
+      <ClassComponents />
 
       {/* <FunctionComponents /> */}
 
@@ -23,7 +23,7 @@ function App() {
 
       {/* <OnChange /> */}
 
-      <OnSubmit />
+      {/* <OnSubmit /> */}
 
 
     </div>
